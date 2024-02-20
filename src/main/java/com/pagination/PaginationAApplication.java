3a338e5834998a3ba8c4b5f03e86a078581921bb
jpavan <jpavan332@gmail.com>
2024-02-19 19:49:29 -0500

    Initial structure

---
+++ src/main/java/com/pagination/PaginationAApplication.java
@@ -0,0 +1,13 @@
+package com.pagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationAApplication.class, args);
+	}
+
+}
