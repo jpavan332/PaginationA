16665d6b9c4e79fb253dc3bb3514eecc6ec49c59
jpavan <jpavan332@gmail.com>
2024-02-22 15:16:36 -0500

    added logger dependency

--- src/main/java/com/pagination/repository/UserRepository.java
+++ src/main/java/com/pagination/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.pagination.repository;
 import com.pagination.Dao.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.awt.print.Pageable;
 
+@Repository
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     Page<User> findByNameContaining(String name, Pageable pageable);
 }
--- src/main/java/com/pagination/service/UserService.java
+++ src/main/java/com/pagination/service/UserService.java
@@ -1,4 +1,24 @@
 package com.pagination.service;
 
-public class UserService {
+import com.pagination.Dao.User;
+import com.pagination.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.awt.print.Pageable;
+
+@Service
+@Transactional
+public class UserService
+{
+    @Autowired
+    UserRepository userRepository;
+    Page<User> getUsers(String name, int page, int size){
+
+            return userRepository.findByNameContaining(name, (Pageable) PageRequest.of(page,size));
+    }
 }
